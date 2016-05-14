396f4c2e669150d6039d75c10f1ddc6d2d68ae26
Liisa Sailaranta <liisa.sailaranta@solita.fi>
2016-05-14 15:26:52 +0300

    Initial commit

---
+++ src/main/java/fi.solita.adele/HelloController.java
@@ -0,0 +1,13 @@
+package fi.solita.adele;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+}
