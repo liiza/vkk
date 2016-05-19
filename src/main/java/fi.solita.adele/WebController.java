211494264497ded85edd9cb049496f700143e84c
Antti Kekki <antti.kekki@solita.fi>
2016-05-19 23:03:25 +0300

    Spring Boot päivitetty 1.2.3 -> 1.3.5. Lisätty Mock-toteutus tilojen statuksen hakuun.

--- src/main/java/fi.solita.adele/WebController.java
+++ src/main/java/fi.solita.adele/WebController.java
@@ -1,5 +1,8 @@
 package fi.solita.adele;
 
+import fi.solita.adele.status.PlaceStatus;
+import fi.solita.adele.status.StatusService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +12,24 @@ import java.util.List;
 @RestController
 public class WebController {
 
-    @Resource private DataService dataService;
+    @Resource
+    private DataService dataService;
 
-    @RequestMapping("/status") String hello() {
+    @Resource
+    private StatusService statusService;
+
+    @RequestMapping("/status")
+    String hello() {
         return "ok";
     }
 
-    @RequestMapping("/all") List<Device> getAllData() {
+    @RequestMapping("/all")
+    List<Device> getAllData() {
         return dataService.all();
+    }
+
+    @RequestMapping(value = "/v1/status/current", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<PlaceStatus> getCurrentStatusForAllPlaces() {
+        return statusService.getCurrentStatusForAllPlaces();
     }
 }
---
+++ src/main/java/fi.solita.adele/status/PlaceStatus.java
@@ -0,0 +1,44 @@
+package fi.solita.adele.status;
+
+public class PlaceStatus {
+    private long place_id;
+    private double latitude;
+    private double longitude;
+    private boolean occupied;
+
+    public long getPlace_id() {
+        return place_id;
+    }
+
+    public PlaceStatus setPlace_id(long place_id) {
+        this.place_id = place_id;
+        return this;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public PlaceStatus setLatitude(double latitude) {
+        this.latitude = latitude;
+        return this;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public PlaceStatus setLongitude(double longitude) {
+        this.longitude = longitude;
+        return this;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public PlaceStatus setOccupied(boolean occupied) {
+        this.occupied = occupied;
+        return this;
+    }
+}
